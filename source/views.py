49124e2d172e0802a9bb42379f8c0c823851bb6b
scotplum <scotplum@gmail.com>
2017-03-12 10:36:43 -0500

    Added API calls without OAUTH to context for source/details

--- source/views.py
+++ source/views.py
@@ -46,14 +46,24 @@ def sourcecontent(request):
     context['feed'] = url_feed
     return render(request, 'source/sourcecontent.html', {'JSON': context})
 
-	
+#The function below parses RSS feeds and API calls	
 def detail(request, source_id):
 	source_object = Source.objects.get(pk=source_id)
+	rss_flag = source_object.rss_flag
 	url = source_object.url
-	url_parsed = feedparser.parse(url)
-	url_parsed0 = url_parsed.entries[0]
-	url_feed = url_parsed.feed
+	oauth_version = source_object.oauth_version
+	sourcename = {'name':source_object.name}
 	context = {}
-	context['entries'] = url_parsed0
-	context['feed'] = url_feed
+	urljson = {}
+	if rss_flag:    
+	    url_parsed = feedparser.parse(url)		
+	    url_parsed0 = url_parsed.entries[0]
+	    url_feed = url_parsed.feed
+	    context['entries'] = url_parsed0
+	    context['feed'] = url_feed
+	#Get api info (need to add code to parse calls with more than one set of data calls that only have 1 set)
+	elif not rss_flag: 
+		urljson = requests.get(url).json()
+		context['api'] = urljson
+	context['source'] = sourcename
 	return render(request, 'source/details.html', {'JSON': context})
